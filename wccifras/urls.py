1c22a5f61fa55a8287ef82e8038404506137e38a
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-10-02 14:52:41 -0300

    Adição de tela de cadastro de cifras

--- wccifras/urls.py
+++ wccifras/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import cifras
+from .views import cifras, cadastrar
 
 urlpatterns = [
     path('', cifras, name='cifras'),
+    path('novo/', cadastrar, name='cadastrar')
 ]
--- wccifras/views.py
+++ wccifras/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def cifras(request):
     return render(request, 'cifras.html')
+
+
+def cadastrar(request):
+    return render(request, 'cadastrar.html')
