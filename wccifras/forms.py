663e6373774ad970ef653679aaff20207b729a27
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-10-28 00:50:20 -0300

    Adaptando formulario de cadastro de cifra, para as tags do django

--- wccifras/forms.py
+++ wccifras/forms.py
@@ -1,8 +1,78 @@
 from django import forms
 from .models import Cifra
+from wcartista.models import Artista
 
 
 class CifraForm(forms.ModelForm):
+    tom_choices = (
+        ('0', 'Selecionar tom'),
+        ('1', 'A'),
+        ('2', 'Bb'),
+        ('3', 'B'),
+        ('4', 'C'),
+        ('5', 'Dd'),
+        ('6', 'D'),
+        ('7', 'Eb'),
+        ('8', 'E'),
+        ('9', 'F'),
+        ('10', 'G'),
+        ('11', 'Ab'),
+    )
+    tom = forms.ChoiceField(choices=tom_choices, initial=0, )
+
+    capotraste_choices = (
+        ('0', 'Sem capotraste'),
+        ('1', '1º Casa'),
+        ('2', '2º Casa'),
+        ('3', '3º Casa'),
+        ('4', '4º Casa'),
+        ('5', '5º Casa'),
+    )
+    capotraste = forms.ChoiceField(choices=capotraste_choices, initial=0, )
+
+    afinacao_choices = (
+        ('0', 'Afinação'),
+        ('1', 'E A D G B E'),
+        ('2', 'Eb Ab Db Gb Bb Eb'),
+        ('3', 'D G C F A D'),
+        ('4', 'Db Gb Cb Fb Ab Db'),
+        ('5', 'C F Bb Eb G C'),
+    )
+    afinacao = forms.ChoiceField(choices=afinacao_choices, initial=0, )
+
+    versao_choices = (
+        ('0', 'Versão'),
+        ('1', 'Ao vivo'),
+        ('2', 'Completa'),
+        ('3', 'Simplificada'),
+        ('4', 'Acústico'),
+        ('5', 'Padrão'),
+    )
+    versao = forms.ChoiceField(choices=versao_choices, initial=0, )
+
+    # wc_artista = forms.ModelChoiceField(queryset=Artista.objects.all())
+
+    nome = forms.CharField(widget=forms.TextInput(attrs={'class': 'single-input', 'placeholder': 'Nome da música*'}))
+
+    genero_choices = (
+        ('0', 'Gênero principal'),
+        ('1', 'Worship'),
+        ('2', 'Clássicos'),
+        ('3', 'Poprock'),
+        ('4', 'Rock'),
+        ('5', 'Reggae'),
+    )
+    genero = forms.ChoiceField(choices=genero_choices, )
+
+    cifra = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 100%; border: none; outline: none; '
+                                                                  'background: #f9f9ff; height: 500px;',
+                                                         'placeholder': 'Transcrição da cifra*'}))
+
+    detalhes = forms.CharField(widget=forms.Textarea(attrs={'style': 'width: 100%; border: none; outline: none; '
+                                                                     'background: #f9f9ff; height: 200px;',
+                                                            'placeholder': 'Deseja enviar algum detalhe adicional '
+                                                                           'para nossa equipe? (Opcional)'}))
+
     class Meta:
         model = Cifra
-        fields = "__all__"
+        fields = ['nome', 'wc_artista', 'genero', 'cifra', 'detalhes', 'tom', 'capotraste', 'afinacao', 'versao', ]
--- wccifras/views.py
+++ wccifras/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import CifraForm
+from .models import Cifra
 
 
 def cifras(request):
@@ -7,10 +8,26 @@ def cifras(request):
 
 
 def cadastrar(request):
-    form = CifraForm(request.POST or None)
+    form = CifraForm()
 
-    if form.is_valid():
-        form.save()
-        redirect('cadastrar_cifra')
+    if request.method == 'POST':
+        form = CifraForm(request.POST)
 
-    return render(request, 'cadastrar.html')
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            wc_artista = form.cleaned_data['wc_artista']
+            genero = form.cleaned_data['genero']
+            cifra = form.cleaned_data['cifra']
+            detalhes = form.cleaned_data['detalhes']
+            tom = form.cleaned_data['tom']
+            capotraste = form.cleaned_data['capotraste']
+            afinacao = form.cleaned_data['afinacao']
+            versao = form.cleaned_data['versao']
+
+            nova_cifra = Cifra(nome='nome', wc_artista='wc_artista', genero='genero', cifra='cifra', detalhes='detalhes', tom='tom', capotraste='capotraste', afinacao='afinacao', versao='versao',)
+
+            nova_cifra.save()
+
+            return redirect('index')
+
+    return render(request, 'cadastrar.html', {'form': form})
