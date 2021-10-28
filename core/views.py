07302bba9733d673ae82337eebc4f9e7992ceb60
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-10-27 22:32:29 -0300

    Retornando no index a consulta das cifras no campo de busca. Necessario criar o template para receber a pesquisa

--- core/views.py
+++ core/views.py
@@ -13,4 +13,10 @@ class Counter:
 def index(request):
     top_cifras = Cifra.objects.all()
 
+    # buscando cifras; preciso alterar para ir para a tela da cifra, mas esta funcionando a busca
+    busca = request.GET.get('buscar_n')
+    if busca:
+        busca_cifra = Cifra.objects.filter(nome__icontains=busca)
+        return render(request, 'index.html', {'top_cifras': busca_cifra, 'top_cifras_count': Counter})
+
     return render(request, 'index.html', {'top_cifras': top_cifras, 'top_cifras_count': Counter})
