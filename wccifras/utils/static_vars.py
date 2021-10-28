12c45451645f107a8742b24e3db538681da2b34a
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-10-28 15:25:59 -0300

    Ajustes cadastro das cifras

--- wccifras/utils/static_vars.py
+++ wccifras/utils/static_vars.py
@@ -2,52 +2,52 @@ class VarsCifraForm:
     # tom
     tom_choices = (
         ('0', 'Selecionar tom'),
-        ('1', 'A'),
-        ('2', 'Bb'),
-        ('3', 'B'),
-        ('4', 'C'),
-        ('5', 'Dd'),
-        ('6', 'D'),
-        ('7', 'Eb'),
-        ('8', 'E'),
-        ('9', 'F'),
-        ('10', 'G'),
-        ('11', 'Ab'),
+        ('A', 'A'),
+        ('Bb', 'Bb'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('Db', 'Db'),
+        ('D', 'D'),
+        ('Eb', 'Eb'),
+        ('E', 'E'),
+        ('F', 'F'),
+        ('G', 'G'),
+        ('Ab', 'Ab'),
     )
     # capotraste
     capotraste_choices = (
         ('0', 'Sem capotraste'),
-        ('1', '1º Casa'),
-        ('2', '2º Casa'),
-        ('3', '3º Casa'),
-        ('4', '4º Casa'),
-        ('5', '5º Casa'),
+        ('1º Casa', '1º Casa'),
+        ('2º Casa', '2º Casa'),
+        ('3º Casa', '3º Casa'),
+        ('4º Casa', '4º Casa'),
+        ('5º Casa', '5º Casa'),
     )
     # afinacao
     afinacao_choices = (
         ('0', 'Afinação'),
-        ('1', 'E A D G B E'),
-        ('2', 'Eb Ab Db Gb Bb Eb'),
-        ('3', 'D G C F A D'),
-        ('4', 'Db Gb Cb Fb Ab Db'),
-        ('5', 'C F Bb Eb G C'),
+        ('E A D G B E', 'E A D G B E'),
+        ('Eb Ab Db Gb Bb Eb', 'Eb Ab Db Gb Bb Eb'),
+        ('D G C F A D', 'D G C F A D'),
+        ('Db Gb Cb Fb Ab Db', 'Db Gb Cb Fb Ab Db'),
+        ('C F Bb Eb G C', 'C F Bb Eb G C'),
     )
     # versao
     versao_choices = (
         ('0', 'Versão'),
-        ('1', 'Ao vivo'),
-        ('2', 'Completa'),
-        ('3', 'Simplificada'),
-        ('4', 'Acústico'),
-        ('5', 'Padrão'),
+        ('Ao vivo', 'Ao vivo'),
+        ('Completa', 'Completa'),
+        ('Simplificada', 'Simplificada'),
+        ('Acústico', 'Acústico'),
+        ('Padrão', 'Padrão'),
     )
     # genero
     genero_choices = (
         ('0', 'Gênero principal'),
-        ('1', 'Worship'),
-        ('2', 'Clássicos'),
-        ('3', 'Poprock'),
-        ('4', 'Rock'),
-        ('5', 'Reggae'),
+        ('Worship', 'Worship'),
+        ('Clássicos', 'Clássicos'),
+        ('Poprock', 'Poprock'),
+        ('Rock', 'Rock'),
+        ('Reggae', 'Reggae'),
     )
 
