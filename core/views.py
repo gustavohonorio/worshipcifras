c5776eecb2b12eb949208516bba9b7b833ecc033
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-11-08 18:23:03 -0300

    Correção do contador top cifras na index

--- core/views.py
+++ core/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render, redirect
 from wccifras.models import Cifra
 
 
-class Counter:
-    count = 0
-
-    def increment(self):
-        self.count += 1
-        return self.count
-
-
 def index(request):
     cifras = Cifra.objects.all()
     top_cifras = Cifra.objects.all()[:5]
@@ -25,4 +17,4 @@ def index(request):
             return redirect('cifras_busca', artista=str(busca_cifra[0].wc_artista).replace(' ', '-').lower(),
                             cifra_id=busca_cifra[0].id, cifra_nome=str(busca_cifra[0].nome).replace(' ', '-').lower())
 
-    return render(request, 'index.html', {'top_cifras': top_cifras, 'top_cifras_count': Counter, 'cifras': cifras})
+    return render(request, 'index.html', {'top_cifras': top_cifras, 'cifras': cifras})
