6f4b8a576b18fa3280ead1197c4c17f90d61250b
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-11-11 00:45:22 -0300

    Criando fronts/back para exibir os dados do banco em tela, no modulo staff

--- wcstaff/urls.py
+++ wcstaff/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import staff
+from .views import staff, artistas, usuarios, cifras
 
 urlpatterns = [
     path('', staff, name='staff'),
+    path('artistas/', artistas, name='s-artistas'),
+    path('cifras/', cifras, name='s-cifras'),
+    path('usuarios/', usuarios, name='s-usuarios'),
 
 ]
--- wcstaff/views.py
+++ wcstaff/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from wcartista.models import Artista
+from wclogon.models import Usuario
+from wccifras.models import Cifra
 
 
 def staff(request):
-    return render(request, 'staff.html')
+    a = Artista.objects.count()
+    c = Cifra.objects.count()
+    u = Usuario.objects.count()
+
+    return render(request, 'staff.html', {'artistas_count': a, 'cifras_count': c, 'usuarios_count': u})
+
+
+def artistas(request):
+    a = Artista.objects.all()
+    return render(request, 's-artistas.html', {'artistas': a})
+
+
+def cifras(request):
+    c = Cifra.objects.all()
+    return render(request, 's-cifras.html', {'cifras': c})
+
+
+def usuarios(request):
+    u = Usuario.objects.all()
+    return render(request, 's-usuarios.html', {'usuarios': u})
+
 
