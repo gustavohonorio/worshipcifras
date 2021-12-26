f85b084b1802a5e6602d3d52dd185d2eeb5259bd
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2021-12-26 01:06:32 -0300

    Usuario pode reportar um bug no link do footer

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.urls import resolve
+
 from wccifras.models import Cifra
 from wcartista.models import Artista
+from wcstaff.forms import ReportErroForm
+from wcstaff.models import ReportErro
 
 
 def index(request):
@@ -8,6 +13,7 @@ def index(request):
     top_cifras = Cifra.objects.all()[:5]
     artistas = Artista.objects.all()
     top_artistas = Artista.objects.all()[:5]
+    form_report = ReportErroForm()
 
     if request.GET.get('buscar_n'):
         if "Buscar" in request.GET.get('buscar_n'):
@@ -26,5 +32,30 @@ def index(request):
                                     cifra_id=buscar_cifra[0].id,
                                     cifra_nome=str(buscar_cifra[0].nome).replace(' ', '-').lower(),)
 
+    # Reportar um bug
+    if request.method == 'POST':
+        if 'report' in request.POST:
+            form_report = ReportErroForm(request.POST)
+            if form_report.is_valid():
+                nome_usuario = request.user.first_name + ' ' + request.user.last_name
+                email_usuario = request.user.email
+                celular_usuario = request.user.celular
+                origem_erro = 'Core - Reportar um bug'
+                detalhes_erro = 'footer'
+                link_erro = resolve(request.path_info).url_name
+                titulo_erro = form_report.cleaned_data['titulo_erro']
+                descricao_erro = form_report.cleaned_data['descricao_erro']
+
+                novo_report = ReportErro(nome_usuario=nome_usuario, email_usuario=email_usuario,
+                                         celular_usuario=celular_usuario, origem_erro=origem_erro,
+                                         detalhes_erro=detalhes_erro, link_erro=link_erro, titulo_erro=titulo_erro,
+                                         descricao_erro=descricao_erro)
+                novo_report.save()
+                messages.success(request, 'Problema reportado. Não se preocupe, nosso time irá analisar o mais rápido'
+                                          ' possivél, e daremos um retorno sobre sua notificação. Obrigado'
+                                          ' por ajudar a comunidade a ser ainda melhor.')
+
+                return redirect('index')
+
     return render(request, 'index.html', {'top_cifras': top_cifras, 'top_artistas': top_artistas, 'cifras': cifras,
-                                          'artistas': artistas})
+                                          'artistas': artistas, 'formReport': form_report, })
