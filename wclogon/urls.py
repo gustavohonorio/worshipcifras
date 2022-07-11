e45c2a61a12b58bfa294500331c51b5d7bffeca9
Gustavo Honorio <gustavo.honorio@neon.com.br>
2022-07-11 17:48:17 -0300

    Feature de redefinição em caso de esquecimento de senha criada. O usuario recebera um email com um codigo (registrado em nova tabela) que tera validade de 24 horas para que seja feita a alteração da senha

--- wclogon/urls.py
+++ wclogon/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import register, meu_perfil, redefinir_senha, esqueci_senha
+from .views import register, meu_perfil, redefinir_senha, esqueci_senha, nova_senha
 
 urlpatterns = [
     path('', register, name='signup'),
     path('register/', register, name='register'),
     path('recuperar-senha/', esqueci_senha, name='esqueci-senha'),
+    path('nova-senha/', nova_senha, name='nova-senha'),
     path('meu-perfil/dados-pessoais/', meu_perfil, name='meu-perfil'),
     path('meu-perfil/redefinir-senha/', redefinir_senha, name='redefinir-senha'),
 ]
