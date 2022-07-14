f5a0164301f4b80d780cf00af36edae6fe93adf6
Gustavo Honorio <gustavo.honorio@neon.com.br>
2022-07-14 12:16:47 -0300

    GM: Funcionalidade para criar um novo ministério; neste momento sera permitido um ministerio por usuario

--- wcministerio/views.py
+++ wcministerio/views.py
@@ -11,54 +11,27 @@ from .forms import MinisterioForm, IntegranteForm
 
 # TODO : ADICAO DE INTEGRANTES NO MINISTERIO
 def ministerios(request):
-    ministerio = Ministerio.objects.filter(owner=request.user.id)
-    m_selecionado = ministerio.order_by('op_data')[:1]
-
     form = MinisterioForm()
-    form_integrante = IntegranteForm()
 
-    if request.GET.get('filter'):
-        m_selecionado = ministerio.filter(nome__icontains=request.GET.get('filter'))
+    ministerio = Ministerio.objects.filter(owner=request.user.id)
 
-        integrantes = []
-
-        for i in MinisterioIntegrantes.objects.filter(co_ministerio=m_selecionado[0].id):
-            user = Usuario.objects.filter(id=i.co_integrante)
-            integrantes.append(user[0].first_name)
-
-        print(integrantes)
+    if ministerio:
+        return render(request, 'meus_ministerios.html', {'form': form, 'ministerio': ministerio[0]})
 
     if request.method == 'POST':
-        if 'convidar-integrante' in request.POST:
-            form_integrante = IntegranteForm(request.POST)
-            if form_integrante.is_valid():
-                email = form_integrante.cleaned_data['email']
+        form = MinisterioForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            descricao = form.cleaned_data['descricao']
+            genero = form.cleaned_data['genero']
 
-                form_integrante.send_email(email, m_selecionado[0].nome, user_functions.usuario_existe(email))
+            novo_ministerio = Ministerio(nome=nome, descricao=descricao, genero=genero, owner=request.user.id,
+                                         co_invite='W' + str(randint(1000, 9999)))
 
-                messages.success(request, 'Convite enviado com sucesso.')
+            novo_ministerio.save()
 
-                return render(request, 'meus_ministerios.html',
-                              {'ministerio': ministerio, 'm_selecionado': m_selecionado,
-                               'integrantes': integrantes, 'form': form,
-                               'form_i': form_integrante})
-        else:
-            form = MinisterioForm(request.POST)
-            if form.is_valid():
-                nome = form.cleaned_data['nome']
-                descricao = form.cleaned_data['descricao']
-                genero = form.cleaned_data['genero']
+            messages.success(request, 'Ministério cadastrado com sucesso.')
 
-                novo_ministerio = Ministerio(nome=nome, descricao=descricao, genero=genero, owner=request.user.id,
-                                             co_invite=str(randint(1000, 9999)))
-                novo_ministerio.save()
+            return render(request, 'meus_ministerios.html', {'form': form, })
 
-                messages.success(request, 'Ministério cadastrado com sucesso.')
-
-                return render(request, 'meus_ministerios.html', {'ministerio': ministerio, 'm_selecionado': m_selecionado,
-                                                                 'form': form,
-                                                                 'form_i': form_integrante})
-
-    return render(request, 'meus_ministerios.html', {'ministerio': ministerio, 'm_selecionado': m_selecionado,
-                                                     'form': form,
-                                                     'form_i': form_integrante})
+    return render(request, 'meus_ministerios.html', {'form': form, })
