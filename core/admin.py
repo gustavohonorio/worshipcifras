2a65a167ce04f5be2aa305c8a67b49fcb266919f
Gustavo Honorio <gustavo.honorio@yahoo.com.br>
2022-10-09 03:50:52 -0300

    Migrando para Bootstrap 5; Alterando home para os cards destaque; Criacao de tabela para gerenciar os cards via UI

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CardDestaque
 
-# Register your models here.
+
+@admin.register(CardDestaque)
+class CardDestaqueAdmin(admin.ModelAdmin):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,7 +1,36 @@
 from django.db import models
+from stdimage.models import StdImageField
+# signals - trata a imagem antes ou depois de inserir na base de dados
+from django.db.models import signals
+# slug - tecnica para altera o nome dos objetos
+from django.template.defaultfilters import slugify
+
+
+class Base(models.Model):
+    status = models.BooleanField('Ativo', default=True)
+    op_create = models.DateField('Data da Criação', auto_now_add=True)
+    op_update = models.DateField('Data da Modificação', auto_now_add=True)
 
 
 class KPI(models.Model):
     id = models.BigAutoField(primary_key=True)
     acessos = models.IntegerField(blank=True, null=True)
     dt_inicio = models.DateField(blank=True, auto_now_add=True)
+
+
+class CardDestaque(Base):
+    titulo = models.CharField('Título', max_length=100)
+    descricao = models.CharField('Descricao', max_length=500)
+    link = models.CharField('Link', max_length=300, null=True)
+    imagem = StdImageField('Imagem', upload_to='.stdimage_cards_destaque')
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.titulo
+
+
+def card_destaque_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.titulo)
+
+
+signals.pre_save.connect(card_destaque_pre_save, sender=CardDestaque)
